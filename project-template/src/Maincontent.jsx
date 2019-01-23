18ec2688197156f9ea25e961cf2623c5a97b8f3b
dhanyn10 <dhanyn.dhan@gmail.com>
2019-01-23 14:17:59 +0700

    project template

---
+++ project-template/src/Maincontent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Maincontent extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+export default Maincontent;
---
+++ project-template/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Maincontent from './index.js';
+
+ReactDOM.render(<Maincontent />, document.getElementById('root'));
