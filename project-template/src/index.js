555371f16a146f4b042543f5cb92225b4f052a55
dhanyn10 <dhanyn.dhan@gmail.com>
2019-01-29 15:36:35 +0700

    update

--- project-template/src/index.js
+++ project-template/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Maincontent from './index.js';
+import Maincontent from './Maincontent.jsx';
 
 ReactDOM.render(<Maincontent />, document.getElementById('root'));
